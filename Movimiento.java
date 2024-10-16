d48df8b3c51f0b6561037e10480fe73c60641a63
Julia Naranjo Gimena <juliagimena00@alumnos.cei.es>
2024-10-16 14:13:00 +0200

    Add files via upload

---
+++ Movimiento.java
@@ -0,0 +1,22 @@
+package movimiento;
+
+public class Movimiento {
+	private float saldoCuenta = 0;
+	private float cantidad;
+	private String concepto;
+	private int idMovimiento = 0;
+	
+	public Movimiento(float saldoCuenta, float cantidad, String concepto, int idMovimiento) {
+		this.saldoCuenta = saldoCuenta;
+		this.cantidad = cantidad;
+		this.concepto = concepto;
+		this.idMovimiento = idMovimiento;	
+	}
+	
+	public void sumarCantidad() {
+		
+		
+		
+	}
+	
+}
---
+++ MovimientosBancarios.java
@@ -0,0 +1,12 @@
+package movimiento;
+
+public class MovimientosBancarios {
+	public static void main (String []args) {
+		
+		
+		
+	}
+	
+	
+	
+}
