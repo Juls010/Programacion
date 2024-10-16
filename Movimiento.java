320bd1645ee000b2575ad326675e319d75210ca9
Julia Naranjo Gimena <juliagimena00@alumnos.cei.es>
2024-10-16 21:20:40 +0200

    Movimiento.java

--- Movimiento.java
+++ Movimiento.java
@@ -1,22 +1,29 @@
 package movimiento;
 
 public class Movimiento {
-	private float saldoCuenta = 0;
+	
+	private static float saldoCuenta = 0;
 	private float cantidad;
 	private String concepto;
-	private int idMovimiento = 0;
-	
-	public Movimiento(float saldoCuenta, float cantidad, String concepto, int idMovimiento) {
-		this.saldoCuenta = saldoCuenta;
-		this.cantidad = cantidad;
-		this.concepto = concepto;
-		this.idMovimiento = idMovimiento;	
+ 	private int idMovimiento = 0;
+ 	
+ 	public Movimientos(String concepto, float cantidad) {
+ 		this.concepto = concepto;
+ 		this.cantidad = cantidad;
+ 		idMovimiento++;
+ 		
+ 	}
+ 	public float ingreso() {
+ 		return saldoCuenta = saldoCuenta + cantidad;
+ 	 }
+ 	public float retirada() {
+ 		return saldoCuenta= saldoCuenta - cantidad;
+ 	}
+ 	public static float getSaldoCuenta() {
+		return saldoCuenta;
 	}
-	
-	public void sumarCantidad() {
-		
-		
-		
+ 	public static void setSaldoCuenta(float saldoCuenta) {
+		saldoCuenta = saldoCuenta;
 	}
 	
 }
